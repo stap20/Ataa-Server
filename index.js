35435c4839018309a514d2954f12ac46a64faf9b
abdom11 <abd123abdo.com@gmail.com>
2024-08-12 16:35:05 +0300

    temp

--- index.js
+++ index.js
@@ -133,5 +133,5 @@ app.post("/logout", (req, res) => {
 // Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}123`);
 });
