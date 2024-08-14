8e2aca2a6323fcacb7a96e52372480c86f563cc1
abdom11 <abd123abdo.com@gmail.com>
2024-08-14 18:12:30 +0300

    Fix console log message in index.js

--- index.js
+++ index.js
@@ -133,5 +133,5 @@ app.post("/logout", (req, res) => {
 // Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}heooll`);
+  console.log(`Server is running on port ${PORT}`);
 });
